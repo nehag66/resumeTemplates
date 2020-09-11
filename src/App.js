c32473a6bce40c9fe816766c2cf51e179fd4c8b8
Neha Goel <nehagoel66@gmail.com>
2020-09-11 19:27:53 +0530

    New project started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
+import Login from './containers/Login/Login';
+import Layout from './containers/Layout/Layout';
+import About from './containers/About/About';
+import Signup from './containers/Signup/Signup';
+import Main from './containers/Main/Main';
+import Template from './containers/Template/Template';
+import Auth from './containers/Auth/Auth';
+import InfoForm from './containers/InfoForm/InfoForm';
+import Template1 from './containers/Template 1/Template1';
+import Template2 from './containers/Template 2/Template2';
+import Template3 from './containers/Template 3/Template3';
+import Template4 from './containers/Template 4/Template4';
+import Contact from './containers/Contact/Contact';
 
-function App() {
+const app = () => {
+
+  const PrivateRoute = ({component: Component, ...rest}) => 
+  (<Route {...rest} render={props => Auth.getAuth()
+    ? (<Component {...props} /> ) 
+    : (<Redirect to={{ pathname: "/" }} /> )
+   } />
+  );
+
+  let routes = (
+
+    <Switch>
+      <Route exact path="/" component={Layout} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <PrivateRoute path="/template" component={Template} />
+      <Route path="/main" component={Main} />
+      <PrivateRoute path="/infoForm" component={InfoForm} />
+      <PrivateRoute path="/template1" component={Template1} />
+      <PrivateRoute path="/template2" component={Template2} />
+      <PrivateRoute path="/template3" component={Template3} />
+      <PrivateRoute path="/template4" component={Template4} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {routes}
     </div>
   );
 }
 
-export default App;
+export default withRouter(app);
